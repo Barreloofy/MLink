02706cb4a4cc411d931c4b5bc850d722b6873213
Barreloofy <nilskofodhk@protonmail.com>
2024-10-02 00:28:01 +0200

    Initial Commit

---
+++ MLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MLink
+//
+//  Created by Barreloofy on 10/2/24 at 12:27 AM.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MLink/MLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLinkApp.swift
+//  MLink
+//
+//  Created by Barreloofy on 10/2/24 at 12:27 AM.
+//
+
+import SwiftUI
+
+@main
+struct MLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
